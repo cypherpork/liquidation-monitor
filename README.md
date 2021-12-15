731bfa4264b8f9bee3fa9a6209823b58ea088850
cypherpork <cypherpork@gmail.com>
2021-12-15 18:50:34 +0300

    add checker and example configuration and small documentation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # oasis-emitter
+You can create events tracking price changes for the currency you are interested in on **oassis.com** and other similar sites
 
+### installing dependencies:
+```pip install selenium``` and
+
+```pip install configparser``` and
+
+and install [geckodriver](https://github.com/mozilla/geckodriver/releases) for you OS
+
+#### Make sure to edit the config file: ```nano example-config.conf``` or ```vim example-config.conf```
+
+#### And rename to **config.conf**: ```mv example-config.conf config.conf```
+
+### Setup linux and Mac
+
+create crontab config file:
+
+```cd ~/``` and
+
+```nano ~/.crontab``` or ```vim ~/.crontab```
+
+Add the check period you need:
+
+```*/5 * * * * python /path/to/directory/scrpt/oasis-emitter/checker.py > /dev/null 2>&1```
+
+here i have set run every 5 minutes
+
+ Update you task list
+
+ ```crontab ~/.crontab```
+
+ and be sure to check that your task is on the list
+
+ ```crontab -l```
---
+++ checker.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import smtplib, ssl, sys, os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from configparser import ConfigParser
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(base_path, "config.conf")
+
+if os.path.exists(config_path):
+	cfg = ConfigParser()
+	cfg.read(config_path)
+else:
+	print("Config not found! Exiting!")
+	sys.exit(1)
+
+show_help_list = cfg.getboolean("main", "show_help_list")
+send_to_info_if_not_alerts = cfg.getboolean("main", "send_to_info_if_not_alerts")
+
+url = cfg.get("web", "url")
+wait = cfg.getfloat("web", "wait")
+
+debug_level = cfg.getint("smtp", "debug_level")
+port = cfg.getint("smtp", "port")
+smtp_server = cfg.get("smtp", "smtp_server")
+sender = cfg.get("smtp", "sender")
+password = cfg.get("smtp", "password")
+receivers = cfg.get("smtp", "receivers").split(",")
+
+def send(sub, text):
+	message = MIMEMultipart()
+	message['From'] = sender
+	message['TO'] = ','.join(receivers)
+	message['Subject'] = sub
+	message['Date'] = formatdate(localtime=True)
+	message.attach(MIMEText(text,'plain'))
+	context = ssl.create_default_context()
+	server = smtplib.SMTP(smtp_server, port)
+	server.set_debuglevel(debug_level)
+	server.starttls()
+	server.login(sender, password)
+	server.sendmail(sender, receivers, message.as_string())
+	server.quit()
+
+
+options = Options()
+options.set_headless()
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--verbose")
+options.add_argument("--headless")
+
+# driver = webdriver.Chrome(executable_path="",options=options)
+
+driver = webdriver.Firefox(options=options)
+
+driver.get(url)
+sleep(wait)
+items = driver.find_elements_by_tag_name("div")[0]
+if show_help_list:
+	print("src list help:")
+	all_items = items.text.split("\n")
+	i = 0
+	while i < len(all_items):
+		print(str(i)+" | "+all_items[i])
+		i += 1
+
+# Liquidation Price value:
+liquid_price_value = items.text.split("\n")[14]
+
+# Current Price
+cur_price_value = items.text.split("\n")[19]
+
+# Next Price:
+next_price_value = items.text.split("\n")[21]
+
+data = "Liquidation Price: "+liquid_price_value+"\r\nCurrent Price: "+cur_price_value+"\r\nNext Price: "+next_price_value+"\r\n"
+
+print("---------------------------------------------------------------")
+print(data)
+
+if next_price_value.replace(',','').replace('$','') < liquid_price_value.replace(',','').replace('$','') :
+	print("important alert!!!")
+	send("important alert!!!", "The liquidation price is greater than the next price\r\n"+data+"\r\n")
+else:
+	print("no alerts...")
+	if send_to_info_if_not_alerts:
+		send("no alerts...", data+"\r\n")
+
+print("---------------------------------------------------------------")
+driver.close()
